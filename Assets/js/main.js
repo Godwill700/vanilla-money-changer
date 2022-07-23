ab07b0a327fb79c36be86612583c41c137a9b16e
Godwill700 <cg22chinyemba@gmail.com>
2022-07-23 19:23:12 +0200

    "+added all calculations for all inputs. +Added animation for total saving and loss. +amended sizing on some break points."

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -4,54 +4,60 @@ const decreaseBtn = document.querySelector("#decrease");
 let rateInput = document.querySelector("#black-market-rate");
 let zwPriceInput = document.querySelector("#zw-price");
 let ratedUsdPrice = document.querySelector("#rated-usd-price");
-// let blackMarketRate = rateInput.value;
-// const billInput = document.querySelector("#bill");
-// let result = document.querySelector("#result");
+let estUsdPriceInput = document.querySelector("#est-usd-price");
+let savingOrLoss = document.querySelector("#total-saving-loss");
 
-console.log(ratedUsdPrice);
+/************************
+ * CALCULATE USD PRICE FORMULA *
+ ************************/
 
-/*********************************
- * SETTING THE BLACK MARKET RATE *
- *********************************/
-setRate = () => {
-  let blackMarketRate = rateInput.value;
+calculateUsdPrice = () => {
+  let blackMarketRate = +rateInput.value;
+  let chargeRate = chargesRange.innerText / 100;
+  let zwPrice = +zwPriceInput.value;
+  let charges = zwPrice * chargeRate;
+  let zwPricePlusCharge = zwPrice + charges;
+  ratedUsdPrice.innerHTML = +(zwPricePlusCharge / blackMarketRate).toFixed(2);
 };
-
-rateInput.addEventListener("change", setRate);
 
 /*******************************
  * SETTING THE CHARGES COUNTER *
  *******************************/
 
-increaseBtn.addEventListener("click", function () {
-  return chargesRange.innerHTML++;
-  console.log(chargesRange);
-});
+decrease = () => {
+  return chargesRange.innerHTML--;
 
-decreaseBtn.addEventListener("click", () => {
-  return chargesRange.innerText--;
-});
-
-/************************
- * SETTING THE ZW PRICE *
- ************************/
-calculateUsdPrice = () => {};
-
-setZwPrice = () => {
-  let zwPrice = +zwPriceInput.value;
-  let blackMarketRate = +rateInput.value;
-  let chargeRate = chargesRange.innerText-- / 100;
-  let charges = zwPrice * chargeRate;
-  let zwPricePlusCharge = zwPrice + charges;
-  console.log(zwPricePlusCharge);
-
-  ratedUsdPrice.innerHTML = +(zwPricePlusCharge / blackMarketRate).toFixed(2);
-
-  console.log(ratedUsdPrice);
-
-  setRate();
-  console.log(blackMarketRate);
   calculateUsdPrice();
 };
 
-zwPriceInput.addEventListener("change", setZwPrice);
+increase = () => {
+  return chargesRange.innerHTML++;
+
+  calculateUsdPrice();
+};
+
+calculateSavingOrLoss = () => {
+  let equivPrice = ratedUsdPrice.innerHTML;
+  savingOrLoss.innerHTML = +(estUsdPriceInput.value - equivPrice).toFixed(2);
+  let totalSavingOrLoss = +savingOrLoss.innerHTML;
+  let totalSaving = document.getElementById("saving");
+  let totalLoss = document.getElementById("loss");
+  console.log(totalSaving);
+  if (totalSavingOrLoss > 0) {
+    totalSaving.style.display = "inline-block";
+    totalSaving.style.marginLeft = "2.5rem";
+  } else {
+    totalLoss.style.display = "inline-block";
+    totalLoss.style.color = "#F71735";
+    totalLoss.style.marginLeft = "2rem";
+  }
+};
+
+zwPriceInput.addEventListener("change", calculateUsdPrice);
+
+rateInput.addEventListener("change", calculateUsdPrice);
+
+increaseBtn.addEventListener("click", increase);
+decreaseBtn.addEventListener("click", decrease);
+
+estUsdPriceInput.addEventListener("change", calculateSavingOrLoss);
